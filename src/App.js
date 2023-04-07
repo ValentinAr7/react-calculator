ab98adcf59aadf7968e40808fd8070798d0e8b12
Valentin <valio990210@gmail.com>
2023-04-07 12:16:27 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 function App() {
   return (
     <div className="App">
+      <div className="calculator"> 
+        <div className="display">
+          <span>(0)</span> 0
 
+          <div className="operators">
+          <button>/</button>
+          <button>*</button>
+          <button>+</button>
+          <button>-</button>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
